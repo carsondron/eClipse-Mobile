4581ccd3b17a4887d474479274920dbdc063ac18
carsondron@gmail.com <carsondron@gmail.com>
2013-02-05 18:57:10 -0600

    Moving URL to index.html

--- eClipse Mobile/scripts/search.js
+++ eClipse Mobile/scripts/search.js
@@ -1,18 +1,27 @@
 
+var searched = false;
+var dsSearch;
 
 function eclipseSearch() {
     var inputText = document.getElementById('txtName');
     
-    var serverURL = "http://localhost/BrokerPlus.WebSL4/ClientBin/BrokerPlus-Web-BrokerPlusDomainService.svc/JSON/";
-    //var serverURL = "http://203.110.139.199/demo/ClientBin/BrokerPlus-Web-BrokerPlusDomainService.svc/JSON/";
+    
     var searchType = document.getElementById('searchType').value;
     
-    switch(searchType)
-    {
-        case "Client":
+    if (searched) {
+        /*dsSearch.options.transport.read.url = serverURL + "GetClientsResult?searchFilter=" + inputText.value;
+        dsSearch.page(1);*/
         
-        	$("#searchResults-listview").kendoMobileListView({
-        		dataSource :{ transport: {
+        var lvSearch = $("#searchResults-listview").data("kendoMobileListView");
+        lvSearch.dataSource.transport.options.read.url = serverURL + "GetClientsResult?searchFilter=" + inputText.value;
+        lvSearch.dataSource.page(1);
+        lvSearch.refresh();
+        //lvSearch.dataSource.read();
+        return;
+    }
+    //searched = true;
+    dsSearch = new kendo.data.DataSource({
+         transport: {
                        read: {
                            url: serverURL + "GetClientsResult?searchFilter=" + inputText.value,
          
@@ -24,8 +33,24 @@ function eclipseSearch() {
                    schema: {
                 data: "GetClientsResultResult.RootResults"
                        }
-                    },
-        		template: $("#clientSearchResults-listview-template").html()
+    });
+         
+    switch(searchType)
+    {
+        case "Client":
+        
+        	$("#searchResults-listview").kendoMobileListView({
+        		dataSource :dsSearch,
+        		template: $("#clientSearchResults-listview-template").html(),
+                 //loadMore: true,
+        click: function (e) {
+            //showActivity(e.dataItem.EventID);
+        },
+        dataBound: function () {
+            if (this.dataSource.total() == 0) {
+                $("#searchResults-listview").html("No Results Found");
+            }
+            }
         	});
         break;
         
